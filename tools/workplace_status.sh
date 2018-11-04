d32d84836e0a732bfbdc6135efa5e0242685e885
Andrew Z Allen <me@andrewzallen.com>
2018-11-04 00:13:34 +0000

    Switch to python for workplace_status script (#153)

--- tools/workplace_status.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-
-echo -n "STABLE_GIT_VERSION "
-if git diff-index --quiet HEAD -- > /dev/null 2>&1; then
-  git describe --tags --abbrev=0
-else
-  printf "%s-dirty\n" "$(git describe --tags --abbrev=0)"
-fi
-
