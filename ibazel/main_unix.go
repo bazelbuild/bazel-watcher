471b9f71a7c0fb5325da00adc974ef0e14ae63b2
John Chadwick <jchw@google.com>
2018-11-10 09:20:26 -0800

    Make setUlimit a noop on Windows.

---
+++ ibazel/main_unix.go
@@ -0,0 +1,40 @@
+// Copyright 2017 The Bazel Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build !windows
+
+package main
+
+import "syscall"
+
+func setUlimit() error {
+	var lim syscall.Rlimit
+
+	err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &lim)
+	if err != nil {
+		return err
+	}
+
+	// set the "soft" file descriptor to the maximum
+	// allowed by a userspace program.
+	// http://man7.org/linux/man-pages/man2/getrlimit.2.html
+	lim.Cur = lim.Max
+
+	err = syscall.Setrlimit(syscall.RLIMIT_NOFILE, &lim)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
