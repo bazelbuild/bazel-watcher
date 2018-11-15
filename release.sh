1e8ce7af6c07e847b0ab20595f77b18901a937be
Andrew Z Allen <me@andrewzallen.com>
2018-11-15 10:56:11 -0700

    Add release script to make releasing easy (#172)

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/usr/bin/env bash
 
 if [ $# -ne "1" ]; then
   cat <<EOF
@@ -18,9 +18,40 @@ EOF
   exit 1
 fi
 
-VERSION="$1"; shift
+set -ex
 
-git tag "${VERSION}"
+GIT_TAG=$1; shift
+
+cat - <<EOF
+Releasing bazel-watcher at tag ${GIT_TAG}.
+
+This will update the changelog, tag a release and release it on NPM.
+
+Press enter to continue
+EOF
+
+read
+
+docker run --rm \
+  --interactive \
+  --tty \
+  -v "${PWD}:/usr/local/src/your-app" \
+  -e "CHANGELOG_GITHUB_TOKEN=${CHANGELOG_GITHUB_TOKEN}" \
+  ferrarimarco/github-changelog-generator:1.14.3 \
+      -u bazelbuild \
+      -p bazel-watcher \
+      --author \
+      --compare-link \
+      --github-site=https://github.com/bazelbuild/bazel-watcher \
+      --unreleased-label "**Next release**" \
+      --future-release="${GIT_TAG}"
+
+# Add the newly generated changelog and commit it.
+git add CHANGELOG.md
+git commit -m "Generating CHANGELOG.md for release ${GIT_TAG}"
+
+# Tag the release.
+git tag "${GIT_TAG}"
 
 if ./npm/publish.sh; then
   # Success! Publish the tag to GitHub
@@ -29,4 +60,3 @@ else
   # Clean up in the event of failure.
   git tag -d "${VERSION}"
 fi
-
