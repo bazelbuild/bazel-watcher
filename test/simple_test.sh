901651e4b04800f6471acaac30bb0806187b0600
Brendan Linn <brendan.linn@gmail.com>
2019-04-14 08:28:06 -0700

    brs/test: add an integration_test.

---
+++ test/simple_test.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+PORT=
+
+# Parse the --backend_port flag.
+while :; do
+  case $1 in
+    --backend_port) PORT=$2; shift ;;
+    *) break ;;
+  esac
+  shift
+done
+
+# Ensure that hello.txt is the same whether read directly from runfiles or indirectly via the
+# server.
+diff brs/test/hello.txt <(curl -s http://localhost:"$PORT"/brs/test/hello.txt)
