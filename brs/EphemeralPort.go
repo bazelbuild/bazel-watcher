bda4fad75e0f1d1ce8004bdc6a8c28d857ffe9ac
Brendan Linn <brendan.linn@gmail.com>
2019-05-21 19:51:22 -0700

    brs: port EphemeralPort to golang.
    inspired by
    https://github.com/phayes/freeport/blob/master/freeport.go.

---
+++ brs/EphemeralPort.go
@@ -0,0 +1,16 @@
+package main
+
+import "net"
+
+func GetEphemeralPort() (int, error) {
+	addr, err := net.ResolveTCPAddr("tcp", "localhost:0")
+	if err != nil {
+		return 0, err
+	}
+	l, err := net.ListenTCP("tcp", addr)
+	if err != nil {
+		return 0, err
+	}
+	defer l.Close()
+	return l.Addr().(*net.TCPAddr).Port, nil
+}
