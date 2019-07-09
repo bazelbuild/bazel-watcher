f090ceabcd16dcc74e5c8c6c5cad89f6d3022518
Luboš Pokorný <lubos.pokorny@gmail.com>
2019-07-09 21:42:45 +0200

    bazel args can be passed to IBazelTester.Run

--- e2e/ibazel.go
+++ e2e/ibazel.go
@@ -43,19 +43,19 @@ func (i *IBazelTester) Build(target string) {
 	i.build(target, []string{})
 }
 
-func (i *IBazelTester) Run(target string) {
-	i.run(target, []string{})
+func (i *IBazelTester) Run(target string, bazelArgs ...string) {
+	i.run(target, []string{}, bazelArgs)
 }
 
 func (i *IBazelTester) RunWithProfiler(target string, profiler string) {
-	i.run(target, []string{"--profile_dev=" + profiler})
+	i.run(target, []string{"--profile_dev=" + profiler}, []string{})
 }
 
 func (i *IBazelTester) RunWithBazelFixCommands(target string) {
 	i.run(target, []string{
 		"--run_output=true",
 		"--run_output_interactive=false",
-	})
+	}, []string{})
 }
 
 func (i *IBazelTester) GetOutput() string {
@@ -157,11 +157,12 @@ func (i *IBazelTester) build(target string, additionalArgs []string) {
 	}
 }
 
-func (i *IBazelTester) run(target string, additionalArgs []string) {
+func (i *IBazelTester) run(target string, additionalArgs []string, bazelArgs []string) {
 	args := []string{"--bazel_path=" + i.bazelPath()}
 	args = append(args, additionalArgs...)
 	args = append(args, "run")
 	args = append(args, target)
+	args = append(args, bazelArgs...)
 	i.cmd = exec.Command(ibazelPath, args...)
 
 	errCode, buildStdout, buildStderr := i.bazel.RunBazel([]string{"build", target})
