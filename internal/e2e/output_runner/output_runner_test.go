fc8a29685bdab5ccc7717e05d03496f1aff1c199
Andrew Z Allen <me@andrewzallen.com>
2022-08-13 14:32:39 -0600

    Update directory structure
    Since 2017 I've learned a lot more about how to structure go projects.
    This is a big refactor to move the things I don't intend to be imported
    by anyone else into an `internal` directory which will prevent external
    parties from importing from it. This gives freedom to manipulate and
    make backward-incompatible API changes inside the `internal` directory
    going forward. Sorry if this breaks you, but it was never the intent of
    the project to create an API that provides programmatic control of
    `bazel`'s CLI. This is a breaking change but not one that I'm going to
    be rolling back because I don't have bandwidth to support that usecase.
    If you're using it as such I would recommend going back to the parent
    commit of this one, copying the directory you need into your project and
    working with it from there.

---
+++ internal/e2e/output_runner/output_runner_test.go
@@ -0,0 +1,180 @@
+package output_runner
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/bazelbuild/bazel-watcher/internal/e2e"
+	"github.com/bazelbuild/rules_go/go/tools/bazel_testing"
+)
+
+const mainFiles = `
+-- single/defs.bzl --
+def fix_deps():
+  print("runacommand")
+-- single/BUILD --
+load("//single:defs.bzl", "fix_deps")
+
+fix_deps()
+
+sh_binary(
+  name = "test",
+  srcs = ["test.sh"],
+)
+
+sh_binary(
+  name = "overwrite",
+  srcs = ["overwrite.sh"],
+)
+-- single/test.sh --
+printf "action"
+-- single/overwrite.sh --
+printf "overwrite"
+-- multiple/defs.bzl --
+def fix_deps():
+  print("runcommand foo")
+  print("runcommand bar")
+  print("runcommand foo")
+  print("runcommand baz")
+-- multiple/BUILD --
+load("//multiple:defs.bzl", "fix_deps")
+
+fix_deps()
+
+sh_binary(
+  name = "test",
+  srcs = ["test.sh"],
+)
+-- multiple/test.sh --
+printf "action"
+`
+
+func TestMain(m *testing.M) {
+	bazel_testing.TestMain(m, bazel_testing.Args{
+		Main: mainFiles,
+	})
+}
+
+func checkNoSentinel(t *testing.T, sentinelFile *os.File, msg string) {
+	t.Helper()
+
+	if _, err := os.Stat(sentinelFile.Name()); !os.IsNotExist(err) {
+		t.Errorf("Found a sentinel when expecting none: %s\n", msg)
+	}
+}
+
+func checkSentinel(t *testing.T, sentinelFile *os.File, msg string) {
+	t.Helper()
+	sentinalFileName := sentinelFile.Name()
+
+	deadline := time.Now().Add(5 * time.Second)
+	var err error
+	for {
+		if time.Now().After(deadline) {
+			t.Errorf("Couldn't find sentinal. os.Stat(%q): %s\n%s\n", sentinalFileName, err, msg)
+			return
+		} else if _, err := os.Stat(sentinalFileName); err == nil {
+			// No error stat'ing the file means it exists.
+			os.Remove(sentinelFile.Name())
+			return
+		}
+	}
+}
+
+func TestOutputRunner(t *testing.T) {
+	sentinelFile, err := ioutil.TempFile("", "fixCommandSentinel")
+	if err != nil {
+		t.Errorf("ioutil.TempFile(\"\", \"fixCommandSentinel\": %v", err)
+	}
+	sentinalFileName := strings.Replace(sentinelFile.Name(), "\\", "/", -1)
+
+	e2e.Must(t, sentinelFile.Close())
+	checkSentinel(t, sentinelFile, "ioutil.TempFile creates the file by default. Delete it.")
+	checkNoSentinel(t, sentinelFile, "The sentinal should now be deleted.")
+
+	// First check that it doesn't run if there isn't a `.bazel_fix_commands.json` file.
+	ibazel := e2e.SetUp(t)
+	ibazel.RunWithBazelFixCommands("//single:overwrite")
+
+	// Ensure it prints out the banner.
+	ibazel.ExpectIBazelError("Did you know")
+
+	e2e.MustWriteFile(t, ".bazel_fix_commands.json", fmt.Sprintf(`
+	[{
+		"regex": "^(.*)runacommand(.*)$",
+		"command": "touch",
+		"args": ["%s"]
+	}]`, sentinalFileName))
+
+	e2e.MustWriteFile(t, "single/overwrite.sh", `
+printf "overwrite1"
+`)
+
+	ibazel.RunWithBazelFixCommands("//single:overwrite")
+
+	ibazel.ExpectOutput("overwrite1")
+	checkSentinel(t, sentinelFile, "The first run should create a sentinel.")
+
+	ibazel.Kill()
+
+	// Invoke the test a 2nd time to ensure it works over multiple separate
+	// invocations of ibazel.
+	ibazel = e2e.SetUp(t)
+	ibazel.RunWithBazelFixCommands("//single:overwrite")
+	ibazel.ExpectOutput("overwrite1")
+	checkSentinel(t, sentinelFile, "The second run should create a sentinel.")
+
+	// TODO: Figure out why the 2nd invocation doesn't touch the file.
+	// Test that the command is run again.
+	//e2e.MustWriteFile(t, "overwrite.sh", `printf "overwrite2"`)
+
+	//ibazel.ExpectOutput("overwrite2")
+	//checkSentinel(t, sentinelFile, "The third run should create a sentinel.")
+
+	// Now replace the print and it shouldn't fire.
+	e2e.MustWriteFile(t, "defs.bzl", `
+def fix_deps():
+  print("not it")
+`)
+
+	ibazel.ExpectOutput("overwrite1")
+	checkNoSentinel(t, sentinelFile, "The third run should not create a sentinel.")
+}
+
+func TestOutputRunnerUniqueCommandsOnly(t *testing.T) {
+	e2e.MustWriteFile(t, ".bazel_fix_commands.json", `
+       [{
+               "regex": "^.*runcommand (.*)$",
+               "command": "echo",
+               "args": ["$1"]
+       }]`)
+
+	ibazel := e2e.NewIBazelTester(t)
+	ibazel.RunWithBazelFixCommands("//multiple:test")
+	defer ibazel.Kill()
+
+	ibazel.ExpectFixCommands([]string{
+		"echo foo",
+		"echo bar",
+		"echo baz",
+	})
+}
+
+func TestNotifyWhenInvalidConfig(t *testing.T) {
+	e2e.MustWriteFile(t, ".bazel_fix_commands.json", `
+invalid json file
+`)
+
+	ibazel := e2e.SetUp(t)
+	ibazel.RunWithBazelFixCommands("//single:test")
+	defer ibazel.Kill()
+
+	// It should run the program and print out an error that says your JSON is
+	// invalid.
+	ibazel.ExpectIBazelError("Error in .bazel_fix_commands.json")
+	ibazel.ExpectOutput("action")
+}
---
+++ internal/ibazel/output_runner/output_runner.go
@@ -0,0 +1,232 @@
+// Copyright 2017 The Bazel Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package output_runner
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/bazelbuild/bazel-watcher/internal/ibazel/log"
+	"github.com/bazelbuild/bazel-watcher/internal/ibazel/workspace"
+	"github.com/bazelbuild/bazel-watcher/third_party/bazel/master/src/main/protobuf/blaze_query"
+)
+
+var (
+	runOutput = flag.Bool(
+		"run_output",
+		true,
+		"Search for commands in Bazel output that match a regex and execute them, the default path of file should be in the workspace root .bazel_fix_commands.json")
+	runOutputInteractive = flag.Bool(
+		"run_output_interactive",
+		true,
+		"Use an interactive prompt when executing commands in Bazel output")
+	notifiedUser = false
+)
+
+// This RegExp will match ANSI escape codes.
+var escapeCodeCleanerRegex = regexp.MustCompile("\\x1B\\[[\\x30-\\x3F]*[\\x20-\\x2F]*[\\x40-\\x7E]")
+
+type OutputRunner struct {
+	w workspace.Workspace
+}
+
+type Optcmd struct {
+	Regex   string   `json:"regex"`
+	Command string   `json:"command"`
+	Args    []string `json:"args"`
+}
+
+func New() *OutputRunner {
+	i := &OutputRunner{
+		w: &workspace.MainWorkspace{},
+	}
+	return i
+}
+
+func (i *OutputRunner) Initialize(info *map[string]string) {}
+
+func (i *OutputRunner) TargetDecider(rule *blaze_query.Rule) {}
+
+func (i *OutputRunner) ChangeDetected(targets []string, changeType string, change string) {}
+
+func (i *OutputRunner) BeforeCommand(targets []string, command string) {}
+
+func (i *OutputRunner) AfterCommand(targets []string, command string, success bool, output *bytes.Buffer) {
+	if *runOutput == false || output == nil {
+		return
+	}
+
+	jsonCommandPath := ".bazel_fix_commands.json"
+	defaultRegex := Optcmd{
+		Regex:   "^buildozer '(.*)'\\s+(.*)$",
+		Command: "buildozer",
+		Args:    []string{"$1", "$2"},
+	}
+
+	optcmd := i.readConfigs(jsonCommandPath)
+	if optcmd == nil {
+		optcmd = []Optcmd{defaultRegex}
+	}
+	commandLines, commands, args := matchRegex(optcmd, output)
+	for idx, _ := range commandLines {
+		if *runOutputInteractive {
+			if i.promptCommand(commands[idx], args[idx]) {
+				i.executeCommand(commands[idx], args[idx])
+			}
+		} else {
+			i.executeCommand(commands[idx], args[idx])
+		}
+	}
+}
+
+func (o *OutputRunner) readConfigs(configPath string) []Optcmd {
+	workspacePath, err := o.w.FindWorkspace()
+	if err != nil {
+		log.Fatalf("Error finding workspace: %v", err)
+		os.Exit(5)
+	}
+
+	jsonFile, err := os.Open(filepath.Join(workspacePath, configPath))
+	if os.IsNotExist(err) {
+		// Note this is not attached to the os.IsNotExist because we don't want the
+		// other error handler to catch if we hav already notified.
+		if !notifiedUser {
+			log.Banner(
+				"Did you know iBazel can invoke programs like Gazelle, buildozer, and",
+				"other BUILD file generators for you automatically based on bazel output?",
+				"Documentation at: https://github.com/bazelbuild/bazel-watcher#output-runner")
+		}
+		notifiedUser = true
+		return nil
+	} else if err != nil {
+		log.Errorf("Error reading config: %s", err)
+		return nil
+	}
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	var optcmd []Optcmd
+	err = json.Unmarshal(byteValue, &optcmd)
+	if err != nil {
+		log.Errorf("Error in .bazel_fix_commands.json: %s", err)
+	}
+
+	return optcmd
+}
+
+func matchRegex(optcmd []Optcmd, output *bytes.Buffer) ([]string, []string, [][]string) {
+	var commandLines, commands []string
+	var args [][]string
+	distinctCommands := map[string]bool{}
+	scanner := bufio.NewScanner(output)
+	for scanner.Scan() {
+		line := escapeCodeCleanerRegex.ReplaceAllLiteralString(scanner.Text(), "")
+		for _, oc := range optcmd {
+			re := regexp.MustCompile(oc.Regex)
+			matches := re.FindStringSubmatch(line)
+			if matches != nil && len(matches) >= 0 {
+				command := convertArg(matches, oc.Command)
+				cmdArgs := convertArgs(matches, oc.Args)
+				fullCmd := strings.Join(append([]string{command}, cmdArgs...), " ")
+				if _, found := distinctCommands[fullCmd]; !found {
+					commandLines = append(commandLines, matches[0])
+					commands = append(commands, command)
+					args = append(args, cmdArgs)
+					distinctCommands[fullCmd] = true
+				}
+			}
+		}
+	}
+	return commandLines, commands, args
+}
+
+func convertArg(matches []string, arg string) string {
+	if strings.HasPrefix(arg, "$") {
+		val, _ := strconv.Atoi(arg[1:])
+		return matches[val]
+	}
+	return arg
+}
+
+func convertArgs(matches []string, args []string) []string {
+	var rst []string
+	for i, _ := range args {
+		var converted strings.Builder
+		converted.Grow(len(args[i]))
+
+		matchIndex := 0
+		matching := false
+
+		writeMatch := func() {
+			if matching {
+				if matchIndex < len(matches) {
+					converted.WriteString(matches[matchIndex])
+				} else {
+					converted.WriteRune('$')
+					converted.WriteString(strconv.Itoa(matchIndex))
+				}
+				matchIndex = 0
+				matching = false
+			}
+		}
+
+		for _, c := range args[i] {
+			if c == '$' {
+				if matching {
+					converted.WriteRune(c)
+				}
+				matching = !matching
+			} else if matching && c >= '0' && c <= '9' {
+				matchIndex = matchIndex*10 + int(c-'0')
+			} else {
+				writeMatch()
+				converted.WriteRune(c)
+			}
+		}
+		writeMatch()
+
+		rst = append(rst, converted.String())
+	}
+	return rst
+}
+
+func (_ *OutputRunner) promptCommand(command string, args []string) bool {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Fprintf(os.Stderr, "Do you want to execute this command?\n%s %s\n[y/N]", command, strings.Join(args, " "))
+	text, _ := reader.ReadString('\n')
+	text = strings.ToLower(text)
+	text = strings.TrimSpace(text)
+	text = strings.TrimRight(text, "\n")
+	if text == "y" {
+		return true
+	} else {
+		return false
+	}
+}
+
+func (o *OutputRunner) executeCommand(command string, args []string) {
+	o.w.ExecuteCommand(command, args)
+}
+
+func (i *OutputRunner) Cleanup() {}
