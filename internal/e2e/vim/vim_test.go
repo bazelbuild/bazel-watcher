d9a2f9376559b6a8e393cbe2ac3c44046d1b621a
Allan Clark <allanc@chickenandporn.com>
2025-03-26 23:14:49 -0700

    chore: timeout adjustment in vim_test

--- internal/e2e/vim/vim_test.go
+++ internal/e2e/vim/vim_test.go
@@ -82,13 +82,13 @@ sh_binary(
 	ibazel := e2e.NewIBazelTester(t)
 	ibazel.Run([]string{}, "//vim:test")
 	defer ibazel.Kill()
-	ibazel.ExpectOutput("Started!", 35 * time.Second)
+	ibazel.ExpectOutput("Started!", 50 * time.Second)
 
 	renameAndWriteNewFile(t, "vim/test.sh", `printf "Started2!"`)
-	ibazel.ExpectOutput("Started2!")
+	ibazel.ExpectOutput("Started2!", 50 * time.Second)
 
 	renameAndWriteNewFile(t, "vim/test.sh", `printf "Started3!"`)
-	ibazel.ExpectOutput("Started3!")
+	ibazel.ExpectOutput("Started3!", 50 * time.Second)
 }
 
 func TestSimpleRunWithModifiedFile_CopyAndTruncWrite(t *testing.T) {
@@ -104,11 +104,11 @@ sh_binary(
 	ibazel := e2e.NewIBazelTester(t)
 	ibazel.Run([]string{}, "//truncate:test")
 	defer ibazel.Kill()
-	ibazel.ExpectOutput("Started!")
+	ibazel.ExpectOutput("Started!", 50 * time.Second)
 
 	copyAndTruncWriteFile(t, "truncate/test.sh", `printf "Started2!"`)
-	ibazel.ExpectOutput("Started2!")
+	ibazel.ExpectOutput("Started2!", 50 * time.Second)
 
 	copyAndTruncWriteFile(t, "truncate/test.sh", `printf "Started3!"`)
-	ibazel.ExpectOutput("Started3!")
+	ibazel.ExpectOutput("Started3!", 50 * time.Second)
 }
