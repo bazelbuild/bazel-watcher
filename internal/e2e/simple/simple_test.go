5ab19a1af3eb801acf420fbd6fe9614bda3ac6ec
Jan Winkler <jan.winkler@gmx.ch>
2025-08-16 12:34:28 +0200

    Add rules_shell to simple test for windows

--- internal/e2e/simple/simple_test.go
+++ internal/e2e/simple/simple_test.go
@@ -14,7 +14,12 @@ import (
 )
 
 const mainFiles = `
+-- MODULE.bazel --
+bazel_dep(name = "rules_shell", version = "0.2.0")
+
 -- BUILD.bazel --
+load("@rules_shell//shell:sh_binary.bzl", "sh_binary")
+
 # Base case test
 sh_binary(
   name = "simple",
